41522dec7505f071f9ceecef863c0b145f89cbe9
Ye Kyaw <yekyawaung@abank.com.mm>
2022-12-05 15:17:18 +0630

    categories

--- lib/base/data_file.dart
+++ lib/base/data_file.dart
@@ -5,7 +5,9 @@ import 'package:hairsalon/app/model/model_category.dart';
 import 'package:hairsalon/app/model/model_profile.dart';
 import 'package:hairsalon/app/model/model_services.dart';
 
-import '../app/model/Taxonomy.dart';
+import '../app/model/Taxonomy.dart' as Taxoomy;
+import '../app/model/BusinessLocation.dart' as BusinessLocation;
+import '../app/model/SalonSpecialists.dart' as SalonSpecialists;
 import '../app/model/model_banner.dart';
 import '../app/model/model_chat.dart';
 import '../app/model/model_intro.dart';
@@ -38,14 +40,103 @@ class DataFile {
   static List<String> getAllTaxonomy() {
     List<String> list = [];
 
-    // list.add(ModelBanner("banner1.png", "#D9EEF9"));
-    // list.add(ModelBanner("banner2.png", "#FFE4EE"));
-    // list.add(ModelBanner("banner3.png", "#F0E4FF"));
-
     return list;
   }
 
-  static Future<List<Data>> callTaxonomy() async {
+  static Future<List<String>> callService() async {
+    try {
+      String uri =
+          'https://pos.crystalshine.net/connector/api/business-location';
+      String token =
+          'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImRmYTVmNTQ3YjBmZjQ5ODU0MmRiNTk2YTI5MzdmYzFmZWQ1MzRjMzI5OGYxNjcxYmVjNGQ0OTA2Y2I1MmMxNTEyMjM4YjkxYTY4NzllZDFhIn0.eyJhdWQiOiI1IiwianRpIjoiZGZhNWY1NDdiMGZmNDk4NTQyZGI1OTZhMjkzN2ZjMWZlZDUzNGMzMjk4ZjE2NzFiZWM0ZDQ5MDZjYjUyYzE1MTIyMzhiOTFhNjg3OWVkMWEiLCJpYXQiOjE2Njk4NzQ0OTgsIm5iZiI6MTY2OTg3NDQ5OCwiZXhwIjoxNzAxNDEwNDk4LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.ST4qUSKtZ5qKxdt_s1nPMN7JqLv82ALhotyW7xDgQRJ_uDD7Mbd1df7aEkSY6Jhplq7XyF_CiRmOGQuunWKaaQgXy6qb4APwsgQyYOw81nYtx6Ss42K8jYPNxZ-WsX2CDC80wp_e17R-_9gCz3poi8DPZ8pTj0M2AzUoBFysgNrqEFjPSOZyuJyYTPs_pQWPBOuSPQLu7TtVTARfNrQNXxLIp37L93bel_sW8OrPsaaZYKNbhgTevGUjTIPq_no0KhqKVILuE4wlD1hlJsfgcDfq1zY9xNDlN4A__sK1tSFcvbUw1hRP_TEJQjJ0xmRrUFwnRTtNnJCH7fcwiVSu_ncMh6HDCn0prqyVu210Z5AUkWiGk1D-SCmnNnrBAIKNLk5SESOEtV_dm0bimuz0PBlUY2zmMTE3a5U3tjttnKLrBycb0sUXxUrzbjJmnsMPlZ8gLZowBfOW8kQFxv-V_YXmR7tSY18hQrCAO_COpYu8P-LAUf6c85kNwhDWMseAhdfs4DtnTVLxUW9qayOLUlNBBcXaoPHQCabIYvSf_1cwDSj4nRKTjBKOxhPQ_p6Al-vkxFpwopqzKM8G3mHYGt6I-QPo8lj-Ay3AMU-lrUWv4DM04NzSHgSnm56tRZfbVSG6tr2o8oozWKsUfWCeHTOvKsUecRoX7Dp2UshYyrI';
+
+      var headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": "Bearer $token",
+      };
+      http.Response response = await http.get(
+        Uri.parse(uri),
+        headers: headers,
+      );
+
+      var tagObjsJson = jsonDecode(response.body)['data'] as List;
+      List<BusinessLocation.Data> tagObjs = tagObjsJson
+          .map((tagJson) => BusinessLocation.Data.fromJson(tagJson))
+          .toList();
+
+      List<String> service = <String>[];
+
+      for (var element in tagObjs) {
+        service.add(element.name);
+      }
+
+      return service;
+      // return tagObjs;
+    } catch (e) {
+      print(e);
+      return [];
+    }
+  }
+
+  static Future<List<SalonSpecialists.Data>> getSalonSpecialists() async {
+    try {
+      String uri = 'https://pos.crystalshine.net/connector/api/user';
+      String token =
+          'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImRmYTVmNTQ3YjBmZjQ5ODU0MmRiNTk2YTI5MzdmYzFmZWQ1MzRjMzI5OGYxNjcxYmVjNGQ0OTA2Y2I1MmMxNTEyMjM4YjkxYTY4NzllZDFhIn0.eyJhdWQiOiI1IiwianRpIjoiZGZhNWY1NDdiMGZmNDk4NTQyZGI1OTZhMjkzN2ZjMWZlZDUzNGMzMjk4ZjE2NzFiZWM0ZDQ5MDZjYjUyYzE1MTIyMzhiOTFhNjg3OWVkMWEiLCJpYXQiOjE2Njk4NzQ0OTgsIm5iZiI6MTY2OTg3NDQ5OCwiZXhwIjoxNzAxNDEwNDk4LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.ST4qUSKtZ5qKxdt_s1nPMN7JqLv82ALhotyW7xDgQRJ_uDD7Mbd1df7aEkSY6Jhplq7XyF_CiRmOGQuunWKaaQgXy6qb4APwsgQyYOw81nYtx6Ss42K8jYPNxZ-WsX2CDC80wp_e17R-_9gCz3poi8DPZ8pTj0M2AzUoBFysgNrqEFjPSOZyuJyYTPs_pQWPBOuSPQLu7TtVTARfNrQNXxLIp37L93bel_sW8OrPsaaZYKNbhgTevGUjTIPq_no0KhqKVILuE4wlD1hlJsfgcDfq1zY9xNDlN4A__sK1tSFcvbUw1hRP_TEJQjJ0xmRrUFwnRTtNnJCH7fcwiVSu_ncMh6HDCn0prqyVu210Z5AUkWiGk1D-SCmnNnrBAIKNLk5SESOEtV_dm0bimuz0PBlUY2zmMTE3a5U3tjttnKLrBycb0sUXxUrzbjJmnsMPlZ8gLZowBfOW8kQFxv-V_YXmR7tSY18hQrCAO_COpYu8P-LAUf6c85kNwhDWMseAhdfs4DtnTVLxUW9qayOLUlNBBcXaoPHQCabIYvSf_1cwDSj4nRKTjBKOxhPQ_p6Al-vkxFpwopqzKM8G3mHYGt6I-QPo8lj-Ay3AMU-lrUWv4DM04NzSHgSnm56tRZfbVSG6tr2o8oozWKsUfWCeHTOvKsUecRoX7Dp2UshYyrI';
+
+      var headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": "Bearer $token",
+      };
+      http.Response response = await http.get(
+        Uri.parse(uri),
+        headers: headers,
+      );
+
+      var tagObjsJson = jsonDecode(response.body)['data'] as List;
+      List<SalonSpecialists.Data> tagObjs = tagObjsJson
+          .map((tagJson) => SalonSpecialists.Data.fromJson(tagJson))
+          .toList();
+
+      return tagObjs;
+    } catch (e) {
+      print(e);
+      return [];
+    }
+  }
+
+  static Future<List<BusinessLocation.Data>> getBusinessLocation() async {
+    try {
+      String uri =
+          'https://pos.crystalshine.net/connector/api/business-location';
+      String token =
+          'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImRmYTVmNTQ3YjBmZjQ5ODU0MmRiNTk2YTI5MzdmYzFmZWQ1MzRjMzI5OGYxNjcxYmVjNGQ0OTA2Y2I1MmMxNTEyMjM4YjkxYTY4NzllZDFhIn0.eyJhdWQiOiI1IiwianRpIjoiZGZhNWY1NDdiMGZmNDk4NTQyZGI1OTZhMjkzN2ZjMWZlZDUzNGMzMjk4ZjE2NzFiZWM0ZDQ5MDZjYjUyYzE1MTIyMzhiOTFhNjg3OWVkMWEiLCJpYXQiOjE2Njk4NzQ0OTgsIm5iZiI6MTY2OTg3NDQ5OCwiZXhwIjoxNzAxNDEwNDk4LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.ST4qUSKtZ5qKxdt_s1nPMN7JqLv82ALhotyW7xDgQRJ_uDD7Mbd1df7aEkSY6Jhplq7XyF_CiRmOGQuunWKaaQgXy6qb4APwsgQyYOw81nYtx6Ss42K8jYPNxZ-WsX2CDC80wp_e17R-_9gCz3poi8DPZ8pTj0M2AzUoBFysgNrqEFjPSOZyuJyYTPs_pQWPBOuSPQLu7TtVTARfNrQNXxLIp37L93bel_sW8OrPsaaZYKNbhgTevGUjTIPq_no0KhqKVILuE4wlD1hlJsfgcDfq1zY9xNDlN4A__sK1tSFcvbUw1hRP_TEJQjJ0xmRrUFwnRTtNnJCH7fcwiVSu_ncMh6HDCn0prqyVu210Z5AUkWiGk1D-SCmnNnrBAIKNLk5SESOEtV_dm0bimuz0PBlUY2zmMTE3a5U3tjttnKLrBycb0sUXxUrzbjJmnsMPlZ8gLZowBfOW8kQFxv-V_YXmR7tSY18hQrCAO_COpYu8P-LAUf6c85kNwhDWMseAhdfs4DtnTVLxUW9qayOLUlNBBcXaoPHQCabIYvSf_1cwDSj4nRKTjBKOxhPQ_p6Al-vkxFpwopqzKM8G3mHYGt6I-QPo8lj-Ay3AMU-lrUWv4DM04NzSHgSnm56tRZfbVSG6tr2o8oozWKsUfWCeHTOvKsUecRoX7Dp2UshYyrI';
+
+      var headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": "Bearer $token",
+      };
+      http.Response response = await http.get(
+        Uri.parse(uri),
+        headers: headers,
+      );
+
+      var tagObjsJson = jsonDecode(response.body)['data'] as List;
+      List<BusinessLocation.Data> tagObjs = tagObjsJson
+          .map((tagJson) => BusinessLocation.Data.fromJson(tagJson))
+          .toList();
+
+      return tagObjs;
+    } catch (e) {
+      print(e);
+      return [];
+    }
+  }
+
+  static Future<List<Taxoomy.Data>> getTaxonomy() async {
     print('hi');
     try {
       String uri = 'https://pos.crystalshine.net/connector/api/taxonomy';
@@ -62,13 +153,9 @@ class DataFile {
         headers: headers,
       );
 
-      print(response.statusCode);
-
-      print("Get");
       var tagObjsJson = jsonDecode(response.body)['data'] as List;
-      print(tagObjsJson);
-      List<Data> tagObjs =
-          tagObjsJson.map((tagJson) => Data.fromJson(tagJson)).toList();
+      List<Taxoomy.Data> tagObjs =
+          tagObjsJson.map((tagJson) => Taxoomy.Data.fromJson(tagJson)).toList();
 
       return tagObjs;
     } catch (e) {
@@ -102,6 +189,7 @@ class DataFile {
     serviceList.add(ModelServices(
         "hair_spa.png", "Hair spa", "Shampoo,spa,blow dry.", "45 min", "150"));
 
+    print(serviceList);
     return serviceList;
   }
 
